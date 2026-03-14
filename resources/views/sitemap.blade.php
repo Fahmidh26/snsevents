be15c4fb63948de4c4c3095586cfcaf682ec4ac6
ifazalam1 <ifaz.alam1@gmail.com>
2026-03-14 17:55:01 +0600

    sitemap and robots.txt updates

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,16 +1,30 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    {{-- Static Routes --}}
+    {{-- Static Routes with appropriate priorities --}}
+    @php
+        $staticMeta = [
+            '/'                    => ['priority' => '1.0', 'changefreq' => 'weekly'],
+            '/services'            => ['priority' => '0.9', 'changefreq' => 'weekly'],
+            '/service-areas'       => ['priority' => '0.9', 'changefreq' => 'monthly'],
+            '/about-us'            => ['priority' => '0.8', 'changefreq' => 'monthly'],
+            '/counseling'          => ['priority' => '0.8', 'changefreq' => 'weekly'],
+            '/management-session'  => ['priority' => '0.7', 'changefreq' => 'weekly'],
+            '/custom-package'      => ['priority' => '0.7', 'changefreq' => 'monthly'],
+            '/privacy-policy'      => ['priority' => '0.3', 'changefreq' => 'yearly'],
+            '/terms-and-conditions'=> ['priority' => '0.3', 'changefreq' => 'yearly'],
+            '/counseling-terms'    => ['priority' => '0.3', 'changefreq' => 'yearly'],
+        ];
+    @endphp
     @foreach ($routes as $route)
+        @php $meta = $staticMeta[$route] ?? ['priority' => '0.6', 'changefreq' => 'monthly']; @endphp
         <url>
             <loc>{{ url($route) }}</loc>
-            <lastmod>{{ now()->toAtomString() }}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.8</priority>
+            <changefreq>{{ $meta['changefreq'] }}</changefreq>
+            <priority>{{ $meta['priority'] }}</priority>
         </url>
     @endforeach
 
-    {{-- Dynamic Events --}}
+    {{-- Dynamic Service/Event Pages --}}
     @foreach ($events as $event)
         <url>
             <loc>{{ route('services.show', $event->slug) }}</loc>
@@ -20,3 +34,4 @@
         </url>
     @endforeach
 </urlset>
+
